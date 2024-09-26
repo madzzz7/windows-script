cdcaa592d1ff0db366caa4e232df336c1f1bb9bd
madzzz7 <matiasmorar20@gmail.com>
2024-09-26 00:28:48 -0300

    Add files via upload

---
+++ windows-script.bat
@@ -0,0 +1,24 @@
+@echo off
+:: Navegar hasta el directorio C:
+cd /d C:\
+
+:: Ir a la carpeta Users
+cd Users
+
+:: Mostrar el contenido de Users para encontrar la carpeta del administrador
+dir
+
+:: Cambiar a la carpeta del administrador (reemplaza 'CarpetaAdmin' con el nombre correcto)
+cd vboxuser
+
+:: Navegar hasta el Escritorio
+cd Desktop
+
+:: Crear el archivo de texto con el contenido
+echo Texto con espacios > NombreArchivo.txt
+
+:: Cambiar la contraseña del usuario (reemplaza 'nombreUsuario' y 'nuevaContraseña')
+net user vboxuser madz
+
+:: Apagar el servidor inmediatamente
+shutdown /s /f /t 0
